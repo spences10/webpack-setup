d8fbd11b986ab0c5a8c70132586b54b258bc667d
spences10 <spences10apps@gmail.com>
2017-07-19 06:57:33 +0100

    prettier

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import { uniq } from 'lodash'
 
-const ages = [1, 1, 1, 22, 2, 2, 33, 3, 3, 3, 4, 44, 4, 44, 4]
+const ages = [ 1, 1, 1, 22, 2, 2, 33, 3, 3, 3, 4, 44, 4, 44, 4 ]
 
 console.log(uniq(ages))
